9c929472941ae63957722f5975254be83b25ea7d
ParasPurwar9554 <paraspurwar5@gmail.com>
2020-12-02 09:28:48 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamicformdemo';
+
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+
+  ngOnInit() {
+  }
+
+  createForm(){
+    this.myForm = this.fb.group({
+	   formArrr: this.fb.array([])
+    });
+    this.addAddress();
+  }
+
+  get formArrr() {
+     return this.myForm.get('formArrr') as FormArray;
+  }
+
+
+ addAddress(){
+      const addressGroup = this.fb.group({
+        'address': [''],
+        'street': [''],
+        'city': [''],
+        'country': ['2']
+      });
+    this.formArrr.push(addressGroup);
+  }
+
+deleteArr(ind){
+	this.formArrr.removeAt(ind);
+}
+
+
+
 }
---
+++ src/app/dynamicform/dynamicform.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-dynamicform',
+  templateUrl: './dynamicform.component.html',
+  styleUrls: ['./dynamicform.component.css']
+})
+export class DynamicformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
